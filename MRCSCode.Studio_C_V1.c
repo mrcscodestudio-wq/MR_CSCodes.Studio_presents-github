4d4d43f7f766c5e0fb43e76f33b4003d012fa043
MR CSCode.Studio <dolasaryan8@gmail.com>
2026-07-11 17:53:53 +0530

    Add first C program that prints messages
    MR CSCode.Studio presents C_V1.c

---
+++ MRCSCode.Studio_C_V1.c
@@ -0,0 +1,17 @@
+//This is my first c program
+/*
+This 
+is 
+my
+first
+c
+program
+*/
+#include <stdio.h>
+int main(){
+    printf("Hi\n");
+    printf("I like pizza\n");
+    printf("Its really good\n");
+    return 0;
+    
+}
