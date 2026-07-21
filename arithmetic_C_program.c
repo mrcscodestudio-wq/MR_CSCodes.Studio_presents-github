783a08f6024f8591a06a98f845a1e80d3291f35f
MR CSCode.Studio <dolasaryan8@gmail.com>
2026-07-21 08:41:28 +0530

    Add arithmetic operations in C program
    arithmetic operations in C program:
    + , - , * , / , % .
    int , float.
    
    -MR CSCode.Studio

---
+++ arithmetic_C_program.c
@@ -0,0 +1,27 @@
+// Arithmetic operations in C
+
+#include <stdio.h>
+
+int main(){
+
+     int a = 8;
+     int b = 19;
+
+     printf("%d\n",a + b);
+     printf("%d\n",a - b);
+     printf("%d\n",a * b);
+     printf("%d\n",a % b);
+     printf("%d\n",a / b);
+
+     float c = 66.77;
+     float d = 124.56;
+
+     printf("%f\n",c + d);
+     printf("%f\n",c - d);
+     printf("%f\n",c * d);
+     printf("%f\n",c / d);
+
+     
+
+    return 0;
+}
